3d8a7751b07f4ec5f57815c0b1833a37f142bf5d
Trevor Smith <tsmit256@uwo.ca>
2019-01-31 13:47:17 -0500

    Assignment 2 moving player

---
+++ tsmit256_Assignment2/Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Declaring gameObject's rigidbody
+    public Rigidbody rb;
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initializing gameobject's rigidbody
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    { 
+        float moveVert = Input.GetAxisRaw("Vertical");
+        float moveHorz = Input.GetAxisRaw("Horizontal");
+
+        Vector3 move = new Vector3(moveHorz, 0, moveVert);
+        rb.AddForce(move * speed);
+    }
+}
