bb8b88476ec6aa3ee9b44f93edd7d190cdf40b51
Trevor Smith <tsmit256@uwo.ca>
2019-02-01 16:01:02 -0500

    Assn2 scoring system

--- tsmit256_Assignment2/Assets/__Scripts/PickUpCreator.cs
+++ tsmit256_Assignment2/Assets/__Scripts/PickUpCreator.cs
@@ -5,63 +5,62 @@ using UnityEngine;
 public class PickUpCreator : MonoBehaviour
 {
     public GameObject cubePrefabVar;
-    public GameObject pickUpObjParent;
+    public GameObject pickUpObjParent; //Name of gameobject drop-down for pickup objects
     public Material colourBlueLight, colourGrey, colourYellow; 
 
-    //NOT SURE IF I NEED THIS LIST              !!!!!!!  !!!   !!!  ! ! ! 
-    public List<GameObject> gameObjectList;
-    public int numCubes = 0;
-    public float ang = 0;
+    private int _numCubes = 0;
+    public int maxNumCubes = 8;
+    private float _ang = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObjectList = new List<GameObject>();
         AddPickUps();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void AddPickUps()
     {
         Vector3 center = new Vector3(0, 1, 0);
-        while (numCubes < 8)
+
+        //While loop that instantiates pick up objects, assigns material, and position
+        while (_numCubes < maxNumCubes)
         {
-            numCubes++;
+            _numCubes++;
             GameObject gObj = Instantiate<GameObject>(cubePrefabVar);
-            gObj.name = "PickUpObj" + numCubes;
+            gObj.name = "PickUpObj" + _numCubes;
+
+            //This places all pick up objects under common gameObject drop-down
             gObj.transform.parent = pickUpObjParent.transform;
 
-            //These if statements determine the material (colour of object)
-            if(numCubes % 3 == 0)
+            //These if-statements determine the material (colour of object)
+            //Material is then assigned name to be used in PlayerController script for scoring purposes
+            if(_numCubes % 3 == 0)
             {
                 gObj.GetComponent<Renderer>().material = colourBlueLight;
+                gObj.GetComponent<Renderer>().material.name = "Light Blue";
             }
-            else if (numCubes % 3 == 1)
+            else if (_numCubes % 3 == 1)
             {
                 gObj.GetComponent<Renderer>().material = colourYellow;
+                gObj.GetComponent<Renderer>().material.name = "Yellow";
             }
             else
             {
                 gObj.GetComponent<Renderer>().material = colourGrey;
+                gObj.GetComponent<Renderer>().material.name = "Grey";
             }
-            
-            gameObjectList.Add(gObj);
             gObj.transform.position = PosOnCircle(center, 3.0f);
         }
     }
 
+    //Generates position of cubes to place them in a circle
     Vector3 PosOnCircle(Vector3 center, float radius)
         {
             Vector3 pos;
-            pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+            pos.x = center.x + radius * Mathf.Sin(_ang * Mathf.Deg2Rad);
             pos.y = center.y;
-            pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
-            ang = ang + 45;
+            pos.z = center.z + radius * Mathf.Cos(_ang * Mathf.Deg2Rad);
+            _ang = _ang + 360/maxNumCubes;
             return pos;
         }
 }
--- tsmit256_Assignment2/Assets/__Scripts/PlayerController.cs
+++ tsmit256_Assignment2/Assets/__Scripts/PlayerController.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     //Declaring gameObject's rigidbody
-    public Rigidbody rb;
+    private Rigidbody _rb;
+
+    public Material colourBlueLight, colourGrey, colourYellow;
+
     public float speed = 5;
+    public int score = 0;
+    public Text scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         //Initializing gameobject's rigidbody
-        rb = gameObject.GetComponent<Rigidbody>();
+        _rb = gameObject.GetComponent<Rigidbody>();
+
+        //Initialize scoreText
+        scoreText.text = "Score: " + score.ToString();
     }
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -24,6 +34,32 @@ public class PlayerController : MonoBehaviour
 
         //Apply commands to a force on object
         Vector3 move = new Vector3(moveHorz, 0, moveVert);
-        rb.AddForce(move * speed);
+        _rb.AddForce(move * speed);
+    }
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUpObj"))
+        {
+            //Deactivates pick up object when collision occurs
+            other.gameObject.SetActive(false);
+
+            //Increment Score (based on property name of material defined in "PickUpCreator" script)
+            if (other.gameObject.GetComponent<Renderer>().material.name == "Yellow")
+            {
+                score = score + 3;
+            }
+            else if (other.gameObject.GetComponent<Renderer>().material.name == "Light Blue")
+            {
+                score = score + 2;
+            }
+            else if (other.gameObject.GetComponent<Renderer>().material.name == "Grey")
+            {
+                score++;
+            }
+            //Display Score
+            scoreText.text = "Score: " + score.ToString();
+        }
     }
 }
