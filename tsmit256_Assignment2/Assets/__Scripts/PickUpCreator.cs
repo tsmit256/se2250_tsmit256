fed6ea82b933738c0f00b61b6c4778b34a0bd8fa
Trevor Smith <tsmit256@uwo.ca>
2019-02-01 14:33:58 -0500

    Assn2 adding pickup objects

---
+++ tsmit256_Assignment2/Assets/__Scripts/PickUpCreator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCreator : MonoBehaviour
+{
+    public GameObject cubePrefabVar;
+    public GameObject pickUpObjParent;
+    public Material colourBlueLight, colourGrey, colourYellow; 
+
+    //NOT SURE IF I NEED THIS LIST              !!!!!!!  !!!   !!!  ! ! ! 
+    public List<GameObject> gameObjectList;
+    public int numCubes = 0;
+    public float ang = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObjectList = new List<GameObject>();
+        AddPickUps();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddPickUps()
+    {
+        Vector3 center = new Vector3(0, 1, 0);
+        while (numCubes < 8)
+        {
+            numCubes++;
+            GameObject gObj = Instantiate<GameObject>(cubePrefabVar);
+            gObj.name = "PickUpObj" + numCubes;
+            gObj.transform.parent = pickUpObjParent.transform;
+
+            //These if statements determine the material (colour of object)
+            if(numCubes % 3 == 0)
+            {
+                gObj.GetComponent<Renderer>().material = colourBlueLight;
+            }
+            else if (numCubes % 3 == 1)
+            {
+                gObj.GetComponent<Renderer>().material = colourYellow;
+            }
+            else
+            {
+                gObj.GetComponent<Renderer>().material = colourGrey;
+            }
+            
+            gameObjectList.Add(gObj);
+            gObj.transform.position = PosOnCircle(center, 3.0f);
+        }
+    }
+
+    Vector3 PosOnCircle(Vector3 center, float radius)
+        {
+            Vector3 pos;
+            pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+            pos.y = center.y;
+            pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+            ang = ang + 45;
+            return pos;
+        }
+}
