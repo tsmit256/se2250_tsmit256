0ff24e097c7365e4d9964a9691bdd717d0f3b45b
Trevor Smith <tsmit256@uwo.ca>
2019-02-28 14:24:14 -0500

    assn3 prospector fix

--- tsmit256_Assignment3/Assets/__Scripts/CardProspector.cs
+++ tsmit256_Assignment3/Assets/__Scripts/CardProspector.cs
@@ -4,34 +4,37 @@ using UnityEngine;
 
 
 
-	// An enum defines a variable type with a few prenamed values        // a
-	public enum eCardState {
-		drawpile,
-		tableau,
-		target,
-		discard
-	}
+// An enum defines a variable type with a few prenamed values        // a
+public enum eCardState
+{
+    drawpile,
+    tableau,
+    target,
+    discard
+}
 
-	public class CardProspector : Card { // Make sure CardProspector extends Card
-		[Header("Set Dynamically: CardProspector")]
-		// This is how you use the enum eCardState
-		public eCardState             state = eCardState.drawpile;
-		// The hiddenBy list stores which other cards will keep this one face down
-		public List<CardProspector>   hiddenBy = new List<CardProspector>();
-		// The layoutID matches this card to the tableau XML if it's a tableau card
-		public int                    layoutID;
-		// The SlotDef class stores information pulled in from the LayoutXML <slot>
-		public SlotDef                slotDef;
+public class CardProspector : Card
+{ // Make sure CardProspector extends Card
+    [Header("Set Dynamically: CardProspector")]
+    // This is how you use the enum eCardState
+    public eCardState state = eCardState.drawpile;
+    // The hiddenBy list stores which other cards will keep this one face down
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    // The layoutID matches this card to the tableau XML if it's a tableau card
+    public int layoutID;
+    // The SlotDef class stores information pulled in from the LayoutXML <slot>
+    public SlotDef slotDef;
 
-		// This allows the card to react to being clicked
-		override public void OnMouseUpAsButton() {
-			// Call the CardClicked method on the Prospector singleton
-			Prospector.S.CardClicked(this);
-		 	
-			base.OnMouseUpAsButton ();
-		                                        
-		}
-	} 
+    // This allows the card to react to being clicked
+    override public void OnMouseUpAsButton()
+    {
+        // Call the CardClicked method on the Prospector singleton
+        Prospector.S.CardClicked(this);
+
+        base.OnMouseUpAsButton();
+
+    }
+}
 
 
 
